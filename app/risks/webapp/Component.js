13b0d8d4edb063190d8c27a17ec52a11c9c651bb
uddiroy <uddiroy@gmail.com>
2021-11-01 05:13:49 +0000

    Push project content to GitHub

---
+++ app/risks/webapp/Component.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+sap.ui.define(["sap/fe/core/AppComponent"], function(AppComponent) {
+    'use strict';
+
+    return AppComponent.extend("riskmanagement.risks.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
